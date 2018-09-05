d8b1b8c182bb638f15ebb6761964183dc6e611ae
Tue Becher <tue@dont-panic.dk>
2018-09-05 23:30:22 +0200

    CountryCode initial commit

---
+++ CountryCode/CountryCode/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("What countrycode should i look up? You can try with dk - Type exit to exit..");
+                string cc = Console.ReadLine().ToLower();
+                if (cc == "exit")
+                {
+                    break;
+                }
+
+                string country;
+                switch (cc)
+                {
+                    case "dk":
+                        country = "Denmark";
+                        break;
+                    case "se":
+                        country = "Sweden";
+                        break;
+                    case "no":
+                        country = "Norway";
+                        break;
+                    default:
+                        country = "unknown";
+                        break;
+                }
+                Console.WriteLine("Country code " + cc + " is " + country);
+            }
+
+        }
+    }
+}
