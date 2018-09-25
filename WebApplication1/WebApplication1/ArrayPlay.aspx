162945bcade7253f47b8255f920e840d361d7690
Tue Becher <tue@dont-panic.dk>
2018-09-25 16:04:44 +0200

    Initial commit ArrayPlay

---
+++ WebApplication1/WebApplication1/ArrayPlay.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ArrayPlay.aspx.cs" Inherits="WebApplication1.ArrayPlay" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:ListBox ID="ListBox1" runat="server" Height="301px" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" Width="560px"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/ArrayPlay.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class ArrayPlay : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            string[] myArray = new string[3];
+            myArray[0] = "Cat";
+            myArray[1] = "Dog";
+            myArray[2] = "Horse";
+
+            AddToListBox(myArray);
+
+            myArray[1] = "Hammy";
+
+            AddToListBox(myArray);
+
+            AddToListBox("Peter", "Poul", "Jan", "Niels");
+        }
+
+        public void AddToListBox(params string[] stringArray)
+        {
+            foreach (string str in stringArray)
+            {
+                ListBox1.Items.Add(str);
+            }
+        }
+
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
