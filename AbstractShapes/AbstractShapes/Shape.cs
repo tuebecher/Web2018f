f1a6f9667a1c00a4450baaef01359ddc5b764878
Tue Becher <tue@dont-panic.dk>
2018-11-13 15:24:38 +0100

    Abstract Shapes starting point

---
+++ AbstractShapes/AbstractShapes/Shape.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AbstractShapes
+{
+    public abstract class Shape
+    {
+        public string Id { get; set; }
+        public Point RefPoint { get; set; }
+        public String Color { get; set; }
+
+        public Shape(string id, Point referencePoint, string color)
+        {
+            Id = id;
+            RefPoint = referencePoint;
+            Color = color;
+        }
+
+        public string GetColor()
+        {
+            return Color;
+        }
+
+        public abstract double GetArea();
+        public abstract double GetCircumference();
+        public abstract double GetWidth();
+        public abstract double GetHeight();
+
+        public override string ToString()
+        {
+            return "[" + Id + ": refpoint=" + RefPoint.ToString() + " area=" + GetArea() + " circ=" + GetCircumference() + "]";
+        }
+    }
+}
---
+++ AbstractShapes/AbstractShapes/WebForm1.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AbstractShapes
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Point p = new Point(4, 23);
+            ListBox1.Items.Add(p.ToString());
+        }
+    }
+}
